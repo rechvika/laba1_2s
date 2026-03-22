49825da5738f9de79b60af02fb147dc218b73d51
Your Name <you@example.com>
2026-03-22 19:18:50 +0700

    remake concatenation and where

--- src/concatenation.c
+++ src/concatenation.c
@@ -1,42 +1,31 @@
 #include "operation.h"
 
-//не копировать поэлементно
 array* concatenation(array* arr_1, array* arr_2, array_errors* error){
     if (arr_1 == NULL || arr_2 == NULL) {
         *error = ARRAY_NOT_DEFINED;
         return NULL;
     }
-    if (arr_1->typeinfo != arr_2->typeinfo) {
+    
+    if (strcmp(arr_1->typeinfo->type_name, arr_2->typeinfo->type_name) != 0) {
         *error = INCOMPATIBLE_TYPES;
         return NULL;
     }
     
     array_errors temp_error;
-    array* arr_new = create_array(arr_1->size + arr_2->size, &temp_error);
+    array* arr_new = create_array(arr_1->size + arr_2->size, arr_1->typeinfo, &temp_error);
     if (temp_error != ARRAY_OPERATION_OK) {
         *error = temp_error;
         return NULL;
     }
 
-    for(unsigned int i = 0; i < arr_1->size; i++){
-        person* elem = (person*)((char*)arr_1->element + (i * arr_1->typeinfo->size));
-        array_add(arr_new, elem, &temp_error);
-        if (temp_error != ARRAY_OPERATION_OK) {
-            free_array(arr_new);
-            *error = temp_error;
-            return NULL;
-        }
-    }
+    memcpy(arr_new->element, arr_1->element, arr_1->size * arr_1->typeinfo->size);
+    arr_new->size += arr_1->size;
 
-    for(unsigned int i = 0; i < arr_2->size; i++){
-        person* elem = (person*)((char*)arr_2->element + (i * arr_2->typeinfo->size));
-        array_add(arr_new, elem, &temp_error);
-        if (temp_error != ARRAY_OPERATION_OK) {
-            free_array(arr_new);
-            *error = temp_error;
-            return NULL;
-        }
-    }
+    memcpy((char*)arr_new->element + (arr_1->size * arr_1->typeinfo->size), 
+           arr_2->element, 
+           arr_2->size * arr_2->typeinfo->size);
+    arr_new->size += arr_2->size;
+    
     *error = ARRAY_OPERATION_OK;
     return arr_new;
 }
--- src/where.c
+++ src/where.c
@@ -1,16 +1,20 @@
 #include "operation.h"
 
-unsigned short age_verification(person* p, array_errors* error) {
-    if (p == NULL) {
-        *error = PERSON_NOT_DEFINED;
+unsigned short age_verification(array* arr, unsigned int i, array_errors* error){
+    if (arr == NULL) {
+        *error = ARRAY_NOT_DEFINED;
         return 0;
     }
-    unsigned short age = 2026 - p->birth.year;
+    if (i >= arr->size) {
+        *error = INDEX_OUT_OF_BOUNDS;
+        return 0;
+    }
+    unsigned short age = 2026 - arr->element[i].birth.year;
     *error = ARRAY_OPERATION_OK;
     return age >= 18;
 }
 
-array* where(array* arr, unsigned short (*function)(person*), array_errors* error){
+array* where(array* arr, unsigned short (*function)(array*, unsigned int, array_errors*), array_errors* error){
     if (arr == NULL) {
         *error = ARRAY_NOT_DEFINED;
         return NULL;
@@ -28,9 +32,7 @@ array* where(array* arr, unsigned short (*function)(person*), array_errors* erro
     }
 
     for(unsigned int i = 0; i < arr->size; i++){
-        person* elem = (person*)((char*)arr->element + (i * arr->typeinfo->size));
-
-                unsigned short condition = function(elem, &temp_error);
+        unsigned short condition = function(arr, i, &temp_error);
         if (temp_error != ARRAY_OPERATION_OK) {
             free_array(where_array);
             *error = temp_error;
@@ -38,6 +40,7 @@ array* where(array* arr, unsigned short (*function)(person*), array_errors* erro
         }
 
         if(condition){
+            person* elem = (person*)((char*)arr->element + (i * arr->typeinfo->size));
             array_add(where_array, elem, &temp_error);
             if (temp_error != ARRAY_OPERATION_OK) {
                 free_array(where_array);
